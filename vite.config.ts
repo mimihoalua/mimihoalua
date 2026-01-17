973c0128cd4e704e045018d82f0283dbf253c74a
mimihoalua <xiemsansang@gmail.com>
2026-01-17 15:21:28 +0700

    Update vite.config.ts
    Signed-off-by: mimihoalua <xiemsansang@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // Cấu hình base này giúp trang web nhận đúng đường dẫn file trên GitHub Pages
-  base: '/mimihoalua/', 
-  
+  // Dev chạy ở '/', Production (GitHub Pages) chạy theo repo name
+  base: mode === "production" ? "/mimihoalua/" : "/",
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
     react(),
     mode === "development" && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
