1dc420cac4580c4d4474502ddb9e4820a19aa75a
mimihoalua <xiemsansang@gmail.com>
2026-01-19 00:16:25 +0700

    Update vite.configold.ts
    Signed-off-by: mimihoalua <xiemsansang@gmail.com>

--- vite.configold.ts
+++ vite.configold.ts
@@ -1,1 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
+export default defineConfig(({ mode }) => ({
+  // Dev chạy ở '/', Production (GitHub Pages) chạy theo repo name
+  base: mode === "production" ? "/mimihoalua/" : "/",
+
+  server: {
+    host: "::",
+    port: 8080,
+  },
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
